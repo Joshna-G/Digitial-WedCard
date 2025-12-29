588ad54399808bb0645767a2f337433a8b5e771a
Joshna G <joshna242004@gmail.com>
2025-12-29 15:14:38 +0530

    changes

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -14,7 +14,6 @@ export default function PricingPage() {
         <main className="relative min-h-screen bg-white">
             <Navbar />
 
-            {/* Hero Section */}
             <section className="pt-32 pb-24 text-center px-4">
                 <motion.div
                     initial={{ opacity: 0, y: 20 }}
